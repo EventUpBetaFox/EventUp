d5f0f8422293b1c75cfcd44bf89e6a2e9c69a07a
Andara <andarasophan@gmail.com>
2020-01-30 23:28:25 +0700

    create event by users

--- routes/event.js
+++ routes/event.js
@@ -1,6 +1,10 @@
 const router = require('express').Router()
 const EventController = require('../controllers/event')
+const UserController = require('../controllers/user')
 
 router.get('/', EventController.findAllEvents)
+
+router.get('/create', UserController.renderCreateEvent)
+router.post('/create', UserController.createEvent)
 
 module.exports = router
